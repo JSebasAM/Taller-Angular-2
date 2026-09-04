b4dcc858d60f5c7a3995d5cc56bfad6886baf630
andres <andres@Machine.Machine>
2026-09-04 12:26:47 -0500

    feat: creación modulo-3

---
+++ src/app/modules/modulo-3/modulo-3-module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component/pokemon-card.component';
+
+
+
+@NgModule({
+  declarations: [
+  
+    PokemonCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Modulo3Module { }
