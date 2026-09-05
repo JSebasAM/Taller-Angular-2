5765d3761ea7a4bbe181fbc673b41adba277b181
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2026-09-04 19:24:09 -0500

    feat: Implementación inicial del modulo 'pedidos' (archivos base)

---
+++ src/app/modules/pedidos/pedidos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos',
+  standalone: false,
+    template: `<H1>Pedidos</H1>`,
+
+})
+export class PedidosComponent {
+
+}
